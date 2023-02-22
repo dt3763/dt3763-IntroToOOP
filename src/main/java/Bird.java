b0f008f572300e30b5e8e7e37dd97573dde5dc9f
dt3763 <dt3763@gmail.com>
2023-02-22 08:20:47 +0000

    birds

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -40,12 +40,22 @@ public class Bird {
     public Bird(){
 
     }
+
     /**
      * @return "Tweet" if ageMonths is less than 3 and "Chirp" if ageMonths is
      *         greater than or equal to 3.
      */
     public String sound(){
-        return "";
+       
+        if (ageMonths < 3){
+        return ("Tweet");}
+
+        else if (ageMonths >= 3){
+            return ("Chirp");
+        }
+        else  
+        return ("sound");}
     }
 
-}
+
+
